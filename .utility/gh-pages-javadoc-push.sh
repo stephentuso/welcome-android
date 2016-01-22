0bed08d64525ff10b9afe43b40adbc1417972225
stephentuso <stephentuso@gmail.com>
2016-01-22 02:53:26 -0500

    Update javadoc push script

--- .utility/gh-pages-javadoc-push.sh
+++ .utility/gh-pages-javadoc-push.sh
@@ -1,26 +1,30 @@
 
 #!/bin/bash
-echo -e "Checking conditions..\n"
-echo "$TRAVIS_REPO_SLUG"
+echo -e "\nChecking conditions.."
 if [ "$TRAVIS_REPO_SLUG" == "stephentuso/welcome-android" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
-  echo -e "Publishing javadoc...\n"
+  echo -e "\nPublishing javadoc..."
 
   cp -R library/build/docs/javadoc $HOME/javadoc-latest
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
+
+  echo -e "\nCloning gh-pages..."
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/stephentuso/welcome-android gh-pages > /dev/null
 
+  echo -e "\nReplacing with updated javadoc..."
   cd gh-pages
   git rm -rf .
   cp -Rf $HOME/javadoc-latest ./
   git add -f .
   git commit -m "Update javadoc (travis build #$TRAVIS_BUILD_NUMBER)"
+
+  echo -e "\nPushing..."
   git push -fq origin gh-pages > /dev/null
 
-  echo -e "Pushed javadoc to gh-pages.\n"
+  echo -e "\nPushed javadoc to gh-pages."
 
 else
     echo -e "Not publishing javadoc.\n"
