082f6c58494a6ace362af792d2fa24e049df17d2
stephentuso <stephentuso@gmail.com>
2016-01-22 02:24:13 -0500

    Add travis javadoc publishing

---
+++ .utility/gh-pages-javadoc-push.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "stephentuso/welcome-android" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing javadoc...\n"
+
+  cp -R library/build/docs/javadoc $HOME/javadoc-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/stephentuso/welcome-android gh-pages > /dev/null
+
+  cd gh-pages
+  git rm -rf .
+  cp -Rf $HOME/javadoc-latest ./
+  git add -f .
+  git commit -m "Update javadoc (travis build #$TRAVIS_BUILD_NUMBER)"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Pushed javadoc to gh-pages.\n"
+
+fi
