0419cf954cb44dd13297fb212ccb00cb7d2332de
stephentuso <stephentuso@gmail.com>
2016-01-22 22:00:51 -0500

    Update WelcomeScreenPage

--- library/src/main/java/com/stephentuso/welcome/ui/WelcomeScreenPage.java
+++ library/src/main/java/com/stephentuso/welcome/ui/WelcomeScreenPage.java
@@ -12,9 +12,29 @@ public class WelcomeScreenPage implements ViewPager.OnPageChangeListener {
     private final BackgroundColor backgroundColor;
     private int index = -2;
 
+    /**
+     * Interface to be implemented by fragments that are part of a WelcomeActivity
+     */
     public interface OnChangeListener {
+
+        /**
+         * // TODO: Fix this for RTL?
+         * Called when this page is coming into view
+         * @param pageIndex The index in the ViewPager of the fragment that implements this
+         * @param offset The % offset of this page, negative if page is off the screen on the right, positive if off on the left
+         * @param offsetPixels The offset of this page in pixels, negative if page is off the screen on the right, positive if off on the left
+         */
         void onScrolled(int pageIndex, float offset, int offsetPixels);
+
+        /**
+         * Called when the fragment that implements this is selected in the ViewPager
+         * @param pageIndex The index of this page
+         */
         void onSelected(int pageIndex);
+
+        /**
+         * Called when the scroll state of the ViewPager changes
+         */
         void onScrollStateChanged(int state);
     }
 
@@ -42,7 +62,7 @@ public class WelcomeScreenPage implements ViewPager.OnPageChangeListener {
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-        if (getFragment() != null && getFragment() instanceof OnChangeListener && index - position <= 1) {
+        if (getFragment() != null && getFragment() instanceof OnChangeListener && position - index <= 1) {
             Fragment fragment = getFragment();
 
             int fragmentWidth = 0;
