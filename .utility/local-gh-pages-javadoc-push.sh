d120560e57aa83a6dfecf37399df5f25f228d07c
stephentuso <stephentuso@gmail.com>
2016-01-25 17:39:04 -0500

    Stop doing automatic javadoc uploads with travis

---
+++ .utility/local-gh-pages-javadoc-push.sh
@@ -0,0 +1,35 @@
+
+#!/bin/bash
+echo "Publishing javadoc..."
+
+WORKINGDIR="$HOME/.gh-pages-javadoc-push"
+
+BUILDDIR="$1"
+if [ $# -eq 0 ]
+  then
+    BUILDDIR="./library/build"
+fi
+
+mkdir $WORKINGDIR
+
+cp -Rf $BUILDDIR/docs/javadoc $WORKINGDIR/javadoc-latest
+
+cd $WORKINGDIR
+
+echo "Cloning gh-pages..."
+git clone --quiet --branch=gh-pages https://$GH_TOKEN@github.com/stephentuso/welcome-android gh-pages > /dev/null
+
+echo "Replacing with updated javadoc..."
+cd gh-pages
+git rm -rf .
+cp -Rf $WORKINGDIR/javadoc-latest/ ./javadoc
+git add -f .
+git commit -m "Update javadoc"
+
+echo "Pushing..."
+git push -fq origin gh-pages > /dev/null
+
+cd $HOME
+rm -rf $WORKINGDIR
+
+echo "Pushed javadoc to gh-pages."
