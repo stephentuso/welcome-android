4327f1525ab48d70050f118e1eb725981f139f01
Brenda So <sogun3@gmail.com>
2016-05-26 02:05:58 -0400

    Added Unit Testing for WelcomeUtils

---
+++ library/src/test/java/com/stephentuso/welcome/test/WelcomeUtilsTest.java
@@ -0,0 +1,31 @@
+package com.stephentuso.welcome.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Brenda on 5/26/2016.
+ */
+public class WelcomeUtilsTest {
+
+    @Test
+    public void testGetKey() throws Exception {
+
+    }
+
+    @Test
+    public void testSetTypeface() throws Exception {
+
+    }
+
+    @Test
+    public void testCalculateParallaxLayers() throws Exception {
+
+    }
+
+    @Test
+    public void testApplyParallaxEffect() throws Exception {
+
+    }
+}
